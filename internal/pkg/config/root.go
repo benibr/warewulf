ce2aed1ab8f1cd88f3762be7072ba006981e4525
Benedikt Braunger <braunger@zib.de>
2024-12-18 22:15:11 +0100

    WIP: renamed

--- internal/pkg/config/root.go
+++ internal/pkg/config/root.go
@@ -29,7 +29,7 @@ var cachedConf WarewulfYaml
 type WarewulfYaml struct {
 	Comment         string        `yaml:"comment,omitempty"`
 	Ipaddr          string        `yaml:"ipaddr,omitempty"`
-	Rripaddr        []string      `yaml:"rripaddr,omitempty"`
+	Rrbootaddrs     []string      `yaml:"rrbootaddrs,omitempty"`
 	Ipaddr6         string        `yaml:"ipaddr6,omitempty"`
 	Netmask         string        `yaml:"netmask,omitempty"`
 	Network         string        `yaml:"network,omitempty"`
--- internal/pkg/overlay/datastructure.go
+++ internal/pkg/overlay/datastructure.go
@@ -24,7 +24,7 @@ type TemplateStruct struct {
 	BuildTimeUnix string
 	BuildSource   string
 	Ipaddr        string
-	Rripaddr      []string
+	Rrbootaddrs   []string
 	Ipaddr6       string
 	Netmask       string
 	Network       string
@@ -72,7 +72,7 @@ func InitStruct(nodeData node.Node) (TemplateStruct, error) {
 	tstruct.Paths = *controller.Paths
 	tstruct.Warewulf = *controller.Warewulf
 	tstruct.Ipaddr = controller.Ipaddr
-	tstruct.Rripaddr = controller.Rripaddr
+	tstruct.Rrbootaddrs = controller.Rrbootaddrs
 	tstruct.Ipaddr6 = controller.Ipaddr6
 	tstruct.Netmask = controller.Netmask
 	tstruct.Network = controller.Network
